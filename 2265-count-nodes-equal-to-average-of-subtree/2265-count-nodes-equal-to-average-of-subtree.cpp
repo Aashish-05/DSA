f822cfc67e346839bf2b352a8640fa7975964a7e
Aashish-05 <mails.aashishpandey@gmail.com>
2026-09-12 01:38:07 +0530

    Time: 11 ms (18.01%), Space: 15.6 MB (84.95%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -6,32 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int averageOfSubtree(TreeNode* root) {
+    int findSum(TreeNode* root,int &cnt){
+        if(root==NULL) return 0;
+        cnt++;
+        int ls = findSum(root->left,cnt);
+        int rs = findSum(root->right,cnt);
+        return ls+rs+root->val;
+    }
+    int solve(TreeNode* root) {
+        int maxi = 0;
         if (root == NULL) return 0;
-
-        int ans = 0;
-
-        function<pair<int,int>(TreeNode*)> solve = [&](TreeNode* node) {
-            if (node == NULL) return make_pair(0, 0);
-
-            auto l = solve(node->left);
-            auto r = solve(node->right);
-
-            int sum = node->val + l.first + r.first;
-            int cnt = 1 + l.second + r.second;
-
-            if (sum / cnt == node->val)
-                ans++;
-
-            return make_pair(sum, cnt);
-        };
-
-        solve(root);
-        return ans;
+        int cnt = 0;
+        int sum = findSum(root,cnt);
+        if(sum/cnt==root->val) maxi++;
+        maxi+=solve(root->left);
+        maxi+=solve(root->right);
+        return maxi;
+    }
+    int averageOfSubtree(TreeNode* root) {
+        int cnt = 0;
+        return solve(root);
     }
 };
